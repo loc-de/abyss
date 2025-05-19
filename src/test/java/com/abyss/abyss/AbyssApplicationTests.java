59b4b54416b4fae425fe25dbc435e6800a3983e9
~ Yurii <locomoc.yt@gmail.com>
2025-05-19 17:49:45 +0300

    init repo

---
+++ src/test/java/com/abyss/abyss/AbyssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abyss.abyss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbyssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
